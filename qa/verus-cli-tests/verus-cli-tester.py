c6e9845ed409d48c7a0adea81c7d031e88b8926c
Asher Dawes <asher.dawes@gmail.com>
2018-06-29 20:50:22 -0700

    Update verus-cli-tester.py

--- qa/verus-cli-tests/verus-cli-tester.py
+++ qa/verus-cli-tests/verus-cli-tester.py
@@ -2,7 +2,7 @@ from subprocess import call, Popen, PIPE, check_output
 import time
 cli_path="../../verus-cli/"
 sleep = 300
-startdaemon = "verusd"
+startdaemon = cli_path + "verusd"
 cli_cmds = "verus getmininginfo", "verus getwalletinfo", "verus stop"
 verusd = Popen(startdaemon, shell=True, close_fds=True)
 time.sleep(sleep)
