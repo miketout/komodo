3b7cfe57630c9a9246c44ce7383b895f16936a60
Asher Dawes <asher.dawes@gmail.com>
2018-06-30 01:16:41 -0700

    Update verus-cli-tester.py

--- qa/verus-cli-tests/verus-cli-tester.py
+++ qa/verus-cli-tests/verus-cli-tester.py
@@ -3,11 +3,10 @@ from time import sleep
 from os import environ, path
 
 seconds = 300
-startdaemon = path.join(environ["CI_PROJECT_DIR"], "verusd")
+startdaemon = "verusd"
 cli_cmds = "verus getmininginfo", "verus getwalletinfo", "verus stop"
 verusd = Popen(startdaemon, shell=True, close_fds=True)
 sleep(seconds)
 for cmd in cli_cmds:  # type: str
-    cmd = path.join(environ["CI_PROJECT_DIR"], cmd)
     with open(path.join(environ["CI_PROJECT_DIR"], "log.txt"), "a") as log:
         log.write(check_output(cmd, shell=True))
