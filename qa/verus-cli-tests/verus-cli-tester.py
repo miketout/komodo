c36912e888305fd3d78ca2d434425a2102f98b1e
Asher Dawes <asher.dawes@gmail.com>
2018-07-01 13:34:30 -0700

    Updating GitLab CI config

--- qa/verus-cli-tests/verus-cli-tester.py
+++ qa/verus-cli-tests/verus-cli-tester.py
@@ -1,11 +1,11 @@
-from subprocess import Popen, check_output
+from subprocess import Popen, check_output, call
 from time import sleep
 from os import environ, path
 
+call("fetch-params", shell=True)
 seconds = 600
-startdaemon = "verusd"
 cli_cmds = ["verus getblockchaininfo", "verus getmininginfo", "verus getwalletinfo", "verus stop"]
-verusd = Popen(startdaemon, shell=True, close_fds=True)
+verusd = Popen("verusd", shell=True, close_fds=True)
 sleep(seconds)
 for cmd in cli_cmds:  # type: str
     with open(path.join(environ["CI_PROJECT_DIR"], "log.txt"), "a") as log:
